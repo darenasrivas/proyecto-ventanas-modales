9deeb9e81ff6f6efeb712fa355987441d363bfc8
David Arenas <darenasrivas@gmail.com>
2024-01-18 13:34:37 +0100

    Creamos las funciones para abrir y cerrar las ventanas modales.

---
+++ script.js
@@ -0,0 +1,18 @@
+// Nos referimos a los elementos para poder cambiar sus clases
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+// Funciones
+
+const openModal = function () {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
